0ed029d1179d0b886e3a13dff6cc6788a9a13a59
Serega <fezarikus@gmail.com>
2023-05-11 17:04:37 +0300

    two commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,24 @@
+import Attachment.*
 fun main() {
     val l = Post.Likes()
-    val post =Post(likes = l)
- println(WallService.add(post))
- println(WallService.upDate(postNew = Post(1,3,4,l)))
-
+    val post = Post(id = null,likes = l, text = null)
+   println(WallService.add(post))
+    println(WallService.upDate(postNew = Post(1, 3, 4, l,text = null)))
 }
 
 object WallService {
     private var posts = mutableListOf<Post>()
     private var counterId = 1
 
-
     fun add(post: Post): Post {
         val changePost = post.copy(id = counterId++)
         posts += changePost
         return posts.last()
     }
+
     fun clear() {
         posts.clear()
         counterId = 1
-        // также здесь нужно сбросить счетчик для id постов, если он у вас используется
     }
 
 
@@ -38,14 +37,16 @@ object WallService {
 
 
 data class Post(
-    val id: Int = 0,
+
+    val id: Int ?,
     val ownerId: Int = 3115, //владелец стены
     val fromId: Int = 3003, // владелец записи
     val likes: Likes,
-    val text: String = " what ?",
+    val text: String ?,
     val friendOnly: Boolean = true, // только для друзей?
     val canPin: Boolean = false, // можешь закрепить запись?
-    val isFavorite: Boolean = false // есть в закладках ?
+    val isFavorite: Boolean = false, // есть в закладках ?
+    val attachments: Array<Attachment> = arrayOf()
 ) {
 
     data class Likes(
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -3,11 +3,11 @@ import Post.*
 import org.junit.Before
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
-import kotlin.test.assertFalse
+
 
 class WallServiceTest {
     private val likes = Likes()
-    private val post = Post(likes = likes)
+    private val post = Post(1,2,3,likes,null)
 
     @Before
     fun clearBeforeTest() {
@@ -16,44 +16,33 @@ class WallServiceTest {
 
     @Test
     fun updateExisting() {
-        // создаём целевой сервис
          WallService
-        // заполняем несколькими постами
+
         WallService.add(post)
         WallService.add(post)
         WallService.add(post)
-        // создаём информацию об обновлении
-        val result =   WallService.upDate(postNew = Post(3,22,222,likes))
 
+        val result =   WallService.upDate(postNew = Post(3,22,222,likes, text = null))
 
-
-        // проверяем результат (используйте assertTrue или assertFalse)
         assertTrue(result)
     }
 
     @Test
     fun add() {
-
         val result = WallService.add(post)
-
         assertEquals(1, result.id)
     }
 
     @Test
     fun upDate() {
-
         val result = WallService.upDate(post)
-
         assertEquals(false, result)
     }
 
     @Test
     fun upDateTwo() {
-
         WallService.add(post)
-        val result = WallService.upDate(postNew = Post(1,3322,30,likes))
-
+        val result = WallService.upDate(postNew = Post(1,3322,30,likes, text = null))
         assertEquals(true, result)
-
     }
 }
